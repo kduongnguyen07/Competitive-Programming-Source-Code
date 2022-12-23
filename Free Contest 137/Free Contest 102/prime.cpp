aae04bd5c106f22a47ecbf93d3d787eff99b34fb
Dương Nguyên Khánh <100816706+kduongnguyen07@users.noreply.github.com>
2022-12-23 08:48:40 +0700

    stupid dog

---
+++ Free Contest 137/Free Contest 102/prime.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e7 + 10;
+bool isPrime[N];
+void Eratos() {
+	for (int i = 2; i < N; ++i) isPrime[i] = true;
+	for (int i = 2; i < N; ++i) if (isPrime[i]) {
+ 		for (long long j = 1,long long * i * i; j < N; j += i) isPrime[j] = false;
+	}
+}
+int main() {
+ int n;
+ cin >> n;
+ Eratos();
+ while (n--) {
+ 	int a, b;
+	cin >> a >> b;
+	long long sum = 0, cnt = 0;
+	for (int i = a; i <= b; ++i) {
+		if (isPrime[i]) sum += i, ++cnt;
+	}
+	if (cnt == 0) cout << 0 <<endl;
+	else cout << fixed << setprecision(2) << 1.0 * sum / cnt <<endl; 
+ }
+}
